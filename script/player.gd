f0533c11749f0168f57fa8478b08158b6b5e15e3
anotida <maltech0005@gmail.com>
2026-09-01 15:02:26 +0200

    manged the intitial setup after creating my own sprites
    setup the game scene, added player and statistics, created a trial run floor

---
+++ script/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var sprite: AnimatedSprite2D = $sprite
+
+const SPEED = 150.0
+
+func _physics_process(delta: float) -> void:
+
+	# Handle jump.
+	var climb := Input.get_axis("up", "down")
+	if climb:
+		velocity.y = climb*SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		if direction<0:
+			sprite.flip_h=true
+		else:
+			sprite.flip_h=false
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()

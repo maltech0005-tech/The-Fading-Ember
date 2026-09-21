cce49b11f8a5a4104318d0ac4d0aea56ec79b691
anotida <maltech0005@gmail.com>
2026-09-21 09:02:33 +0200

    designed the shop scene, and adjusted a few thing on the setting scene
    the items in the setting menu weren't aligning, the shop scene is now loooking promising now onto a few touch ups on all before i finish the help scene

--- script/game.gd
+++ script/game.gd
@@ -79,4 +79,4 @@ func _on_restart_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
